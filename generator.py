5b2747ddaf5cac2dd9f26f4b13ed88d3ac40c920
idk <74161148+heywhyyoulookinghere@users.noreply.github.com>
2022-12-11 11:47:20 +0000

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,12 +1,20 @@
 import string
 import random
 
-def generate_key(length):
-  chars = string.ascii_letters
-  key = ''.join(random.choice(chars) for i in range(length))
+def generate_key(needed_length):
+  if isinstance(needed_length, int):
+    chars = string.ascii_letters
+    key = ''.join(random.choice(chars) for i in range(needed_length))
 
-  return key
+    return key
 
-key = generate_key(100)
+needed_length = None
 
-print(key)
+try:
+  needed_length = int(input("Please enter a valid length: "))
+
+  if isinstance(needed_length, int):
+    key = generate_key(needed_length)
+    print(key)
+except ValueError:
+  print("Invalid input, Please enter a valid number.")
