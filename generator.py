33ea829e7cdae2eee978871b87834a06786cf49a
idk <74161148+heywhyyoulookinghere@users.noreply.github.com>
2022-12-11 11:25:54 +0000

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,12 @@
+import string
+import random
+
+def generate_key(length):
+  chars = string.ascii_letters + string.digits + string.punctuation
+  key = ''.join(random.choice(chars) for i in range(length))
+
+  return key
+
+key = generate_key(16)
+
+console.log(key)
